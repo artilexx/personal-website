7e034a5f7c097bffc2b401d383b97b339eec7a2d
artilexx <97213042+artilexx@users.noreply.github.com>
2022-06-19 02:10:34 -0400

    safari topbar fix #2

--- static/functions.js
+++ static/functions.js
@@ -2,7 +2,7 @@ function navBarScroll() {
     var prevScrollpos = window.pageYOffset;
     window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos && currentScrollPos >= 0) {
+    if (prevScrollpos > currentScrollPos && currentScrollPos >= 0 && prevScrollpos >= 0) {
         document.getElementById("navbar").style.top = "0";
     } else {
         document.getElementById("navbar").style.top = "-50px";
